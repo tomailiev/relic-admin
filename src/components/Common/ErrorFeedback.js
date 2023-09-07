0404b53dbb5223a8002783cc9a0b55bf2eb2e696
Toma Iliev <iliev.toma@gmail.com>
2023-09-06 17:15:01 -0700

    err alert capitalized

--- src/components/Common/ErrorFeedback.js
+++ src/components/Common/ErrorFeedback.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import ErrorContext from "../../context/ErrorContext";
-import { Alert, Snackbar } from "@mui/material";
+import { Alert, AlertTitle, Snackbar } from "@mui/material";
 
 const ErrorFeedback = () => {
 
@@ -11,11 +11,12 @@ const ErrorFeedback = () => {
     return (
         <Snackbar
             open={!!error}
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
             onClose={handleClose}
             autoHideDuration={6000}
         >
             <Alert onClose={handleClose} severity={error?.severity} sx={{ width: '100%' }}>
+                <AlertTitle sx={{ textTransform: 'capitalize' }}>{error?.severity}</AlertTitle>
                 {error?.message || 'Something went'}
             </Alert>
         </Snackbar>
