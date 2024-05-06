de09a6e51f01664593270ea8a22e89b508afcd36
Toma Iliev <iliev.toma@gmail.com>
2024-05-05 21:50:17 -0400

    added tiers

--- src/props/donorProps.js
+++ src/props/donorProps.js
@@ -1,6 +1,34 @@
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
+const getTier = (amount) => {
+    return amount >= 50 && amount < 200
+        ? 'Muse $50+'
+        : amount >= 200 && amount < 500
+            ? 'Dionysus $200+'
+            : amount >= 500 && amount < 1000
+                ? 'Artemis $500+'
+                : amount >= 1000 && amount < 2500
+                    ? 'Hermes $1000+'
+                    : amount >= 2500 && amount < 7500
+                        ? 'Athena $2500+'
+                        : amount >= 7500 && amount < 15000
+                            ? 'Apollo $7500+'
+                            : amount >= 15000
+                                ? 'Zeus & Hera $15,000+'
+                                : ''
+};
+
+const today = new Date();
+const isYearEnd = (today.getUTCMonth()) > 5;
+const startDate = isYearEnd
+    ? new Date(`07-01-${today.getUTCFullYear()}`)
+    : new Date(`07-01-${today.getUTCFullYear() - 1}`);
+
+const endDate = isYearEnd
+    ? new Date(`06-30-${today.getUTCFullYear() + 1}`)
+    : new Date(`06-30-${today.getUTCFullYear()}`);
+
 const donorColumns = [
     { field: 'firstName', headerName: 'First name', flex: 1 },
     { field: 'lastName', headerName: 'Last name', flex: 1 },
@@ -26,22 +54,30 @@ const donorColumns = [
         sortComparator: (v1, v2) => Number(v1.substring(1)) - Number(v2.substring(1)),
     },
     {
+        field: 'yearEndStatus',
+        headerName: 'Current status',
+        valueGetter: ({ row }) => getTier(row.donations
+            .filter(donation => new Date(donation.date) >= startDate && new Date(donation.date <= endDate))
+            .reduce((a, c) => a + c.amount, 0)),
+        flex: 2
+    },
+    {
         field: 'lastRecognitionName',
         headerName: 'Recognition',
         valueGetter: ({ row }) => `${row.donations[row.donations.length - 1].recognitionName}`,
         flex: 2
     },
-    {
-        field: 'type',
-        headerName: 'Type',
-        valueGetter: ({ row }) => {
-            return Array.from(row.donations?.reduce((acc, curr) => {
-                acc.add(curr.campaign);
-                return acc;
-            }, new Set())).join(', ');
-        },
-        flex: 1.5
-    },
+    // {
+    //     field: 'type',
+    //     headerName: 'Type',
+    //     valueGetter: ({ row }) => {
+    //         return Array.from(row.donations?.reduce((acc, curr) => {
+    //             acc.add(curr.campaign);
+    //             return acc;
+    //         }, new Set())).join(', ');
+    //     },
+    //     flex: 1.5
+    // },
     {
         field: 'select',
         headerName: 'Select',
