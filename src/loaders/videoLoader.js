f211b608d97162a9c5002475c117b173e2987975
Toma Iliev <iliev.toma@gmail.com>
2025-01-03 12:16:22 -0500

    campaign spam

--- src/loaders/videoLoader.js
+++ src/loaders/videoLoader.js
@@ -2,5 +2,5 @@ import { downloadDocsV2 } from "../utils/firebase/firebase-functions";
 import collections from "../vars/collections";
 
 export default async function videoLoader() {
-    return await downloadDocsV2(collections.videos, [{ value: ['featured', '!=', 0], type: 'condition' }, { value: ['featured', 'desc'], type: 'sorting' }]);
+    return await downloadDocsV2(collections.videos, [{ value: ['featured', 'desc'], type: 'sorting' }]);
 }
