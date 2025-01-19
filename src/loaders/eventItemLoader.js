28e36ca13499b43e6f1320b6eef0220e33850dc4
Toma Iliev <iliev.toma@gmail.com>
2025-01-18 22:14:47 -0500

    fix/event/loader-program-fix

--- src/loaders/eventItemLoader.js
+++ src/loaders/eventItemLoader.js
@@ -6,6 +6,6 @@ export default async function eventItemLoader({ params }) {
     const doc = await downloadOneDoc(collections.events, params.eventId);
     if (!doc) return null;
     const imgSrc = new File([await getLink(doc.imageUrl)], doc.imageUrl.substring(doc.imageUrl.lastIndexOf('/') + 1));
-    const programBook = new File([await getLink(doc.program)], doc.program.substring(doc.program.lastIndexOf('/') + 1));
+    const programBook = doc.program && new File([await getLink(doc.program)], doc.program.substring(doc.program.lastIndexOf('/') + 1));
     return Object.assign(deschematifyEvent(doc), { imgSrc, programBook })
 }
