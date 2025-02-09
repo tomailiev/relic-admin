21764706e2ba9a3d8d4f080ae359ea56bf481f60
Toma Iliev <iliev.toma@gmail.com>
2025-02-08 22:40:51 -0500

    feature/lists/loader-action-updates

--- src/loaders/listItemLoader.js
+++ src/loaders/listItemLoader.js
@@ -1,7 +1,8 @@
-import { downloadOneDoc } from "../utils/firebase/firebase-functions";
+import { downloadDocsV2, downloadOneDoc } from "../utils/firebase/firebase-functions";
 import collections from "../vars/collections";
 
 export default async function listItemLoader({ params }) {
     const item = await downloadOneDoc(collections.lists, params.listId);
-    return { ...item, newMembers: item.members };
+    const members = await downloadDocsV2(collections[item.source], [{type: 'condition', value: ['email', 'in', item.members]}])
+    return { ...item, newMembers: members };
 }
