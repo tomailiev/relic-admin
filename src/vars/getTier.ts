69e7493aa49f2df22eae9a92806436a4694a3efb
Toma Iliev <iliev.toma@gmail.com>
2025-08-27 14:19:18 -0400

    fix/zeus-fix

--- src/vars/getTier.ts
+++ src/vars/getTier.ts
@@ -1,5 +1,6 @@
 export const getTier = (amount: number) => {
-    return amount > 0 && amount < 250
+    return amount <= 0 ? ''
+        : amount > 0 && amount < 250
             ? 'Muse $0+'
             : amount >= 250 && amount < 500
                 ? 'Dionysus $250+'
@@ -7,7 +8,7 @@ export const getTier = (amount: number) => {
                     ? 'Artemis $500+'
                     : amount >= 1000 && amount < 5000
                         ? 'Hermes $1000+'
-                            : amount >= 5000 && amount < 10000
-                                ? 'Apollo $5000+'
-                                : 'Zeus & Hera $10,000+';
+                        : amount >= 5000 && amount < 10000
+                            ? 'Apollo $5000+'
+                            : 'Zeus & Hera $10,000+';
 };
